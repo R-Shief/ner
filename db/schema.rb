cbb4131876f8489f78b7f30d448b48cd7b07be74
Mahmoud Said <mahmoud@modsaid.com>
2013-11-01 15:26:38 +0200

    adding active admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101125426) do
+ActiveRecord::Schema.define(version: 20131101132554) do
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "category", primary_key: "cat_id", force: true do |t|
     t.binary  "cat_title",   limit: 255,                 null: false
@@ -48,6 +81,25 @@ ActiveRecord::Schema.define(version: 20131101125426) do
   add_index "externallinks", ["el_from", "el_to"], name: "el_from", length: {"el_from"=>nil, "el_to"=>40}, using: :btree
   add_index "externallinks", ["el_index"], name: "el_index", length: {"el_index"=>60}, using: :btree
   add_index "externallinks", ["el_to", "el_from"], name: "el_to", length: {"el_to"=>60, "el_from"=>nil}, using: :btree
+
+  create_table "page", primary_key: "page_id", force: true do |t|
+    t.integer "page_namespace",                    default: 0,     null: false
+    t.binary  "page_title",            limit: 255,                 null: false
+    t.binary  "page_restrictions",     limit: 255,                 null: false
+    t.integer "page_counter",          limit: 8,   default: 0,     null: false
+    t.boolean "page_is_redirect",                  default: false, null: false
+    t.boolean "page_is_new",                       default: false, null: false
+    t.float   "page_random",                       default: 0.0,   null: false
+    t.binary  "page_touched",          limit: 14,                  null: false
+    t.integer "page_latest",                       default: 0,     null: false
+    t.integer "page_len",                          default: 0,     null: false
+    t.boolean "page_no_title_convert",             default: false, null: false
+  end
+
+  add_index "page", ["page_is_redirect", "page_namespace", "page_len"], name: "page_redirect_namespace_len", using: :btree
+  add_index "page", ["page_len"], name: "page_len", using: :btree
+  add_index "page", ["page_namespace", "page_title"], name: "name_title", unique: true, using: :btree
+  add_index "page", ["page_random"], name: "page_random", using: :btree
 
   create_table "page_texts", force: true do |t|
     t.integer  "page_id"
