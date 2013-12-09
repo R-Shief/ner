e364b81f78cff303627448b29266fc2072471d66
Mahmoud Said <mahmoud@modsaid.com>
2013-12-09 21:05:09 +0200

    enhancing form clear

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -6,20 +6,25 @@ var process = function(query){
   $.post(url,data,function(data,status,xhr){
     if (data.status == "Error"){
        $("#api-results-summary").html("Error: " + data.error_message);
+       $("#api-demo-results").hide();
        return false;
     }
     entities_found = data.entities.length;
     if(entities_found >0){
       $("#api-results-summary").html(entities_found + " matching topics has been found");
       $("#api-demo-results").show();
+      $("#api-demo-results tbody").html("");
+      console.log(entities_found);
       for(var i=0; i<entities_found; i++) {
         var entity = data.entities[i];
         console.log(entity.name + " :    "+ entity.brief);
         $.tmpl( "<tr><td>${name}</td><td>${brief}</td><td>${type}</td></tr>", 
-                {"name": entity.name, "brief": entity.brief, "type": "Entity" }).appendTo( "#api-demo-results" );
+                {"name": entity.name, "brief": entity.brief, "type": "Entity" }).
+              appendTo("#api-demo-results tbody");
       }       
     }else{
       $("#api-results-summary").html("No matching topics has been found");
+      $("#api-demo-results").hide();
     }
   },dataType);
 }
@@ -30,5 +35,9 @@ $(document).ready(function(){
     console.log("query is "+query);
     process(query);
     return false;
+  });
+  $("#api-demo-form").bind('reset',function(){
+    $("#api-demo-results").hide();
+    $("#api-results-summary").html("");
   })
 })
